d8a0e4077a06b8994fcdfb30d5985ec92aba5872
ExgleGaming <EaglesFan91x@outlook.com>
2026-04-03 21:07:17 -0400

    health component

---
+++ scripts/components/health_component.gd
@@ -0,0 +1,34 @@
+class_name HealthComponent extends Node
+
+
+signal on_unit_damaged(amount: float)
+signal on_unit_healed(amount: float)
+signal on_unit_dead
+
+var current_health: float
+var max_health: float
+
+
+func init_health(value: float) -> void:
+	current_health = value
+	max_health = value
+
+
+func take_damage(value: float) -> void:
+	if current_health > 0:
+		current_health -= value
+		on_unit_damaged.emit(value)
+	
+	if current_health <= 0:
+		die()
+
+
+func die() -> void:
+	current_health = 0.0
+	on_unit_dead.emit()
+
+
+func heal(value: float) -> void:
+	if current_health >= max_health: return
+	current_health = min(max_health, current_health + value)
+	on_unit_healed.emit(value)
